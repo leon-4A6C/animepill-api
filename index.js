2862cf8a5eb4bc744757e26e7d2abe14aae65e49
leon in 't Veld <leon3110l@gmail.com>
2017-08-30 14:05:02 +0200

    fixed a bug

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ class Anime {
   }
 
   _get(path, options, param) {
+    if(path.search(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/) != -1) {
+      // url is passed
+      return fetch(path)
+        .then(res => res.text())
+    }
     return fetch(this.base + this._parsePathParam(path, param) + this._genOptions(options))
       .then(res => res.text())
   }
@@ -96,7 +101,7 @@ class Anime {
                 href: href,
                 episode: episode,
                 slug: slug,
-                getEpisode: () => this.getEpisode(slug, episode)
+                getEpisode: () => this.getEpisode(href)
               };
             });
             resolve(output);
@@ -113,7 +118,13 @@ class Anime {
    * @returns {Promise<Object[]>} an array with the episode mp4 uri
    */
   getEpisode(slug, ep) {
-    return this._get(endpoints.ep, {}, {slug: slug, episode: ep})
+    let endpoint = endpoints.ep;
+    if(slug.search(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/) != -1) {
+      // url is passed
+      endpoint = slug;
+    }
+    
+    return this._get(endpoint, {}, {slug: slug, episode: ep})
       .then(html => {
         const $ = cheerio.load(html);
         return fetch($("#video-iframe").attr("src"))
