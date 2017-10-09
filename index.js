ef3493a81a64981f9e6ba36ac5fa7a6bc23b6966
leon in 't Veld <leon3110l@gmail.com>
2017-10-09 20:52:19 +0200

    added prefix in the constructor

--- index.js
+++ index.js
@@ -1,60 +1,73 @@
-const fetch = require("node-fetch");
-const cheerio = require("cheerio");
-const endpoints = require("./endpoints");
+const fetch = require('node-fetch')
+const cheerio = require('cheerio')
+const endpoints = require('./endpoints')
 
 class Anime {
   /**
    * constructor, instantiates the object
-   * @param {string} url - the base url to use, default: http://animepill.com
+   * @param {string} prefix - it uses this in front of the request, you could use this to prevent cors errors in browsers
+   * @param {string} url - the url to make request to, default: http://animepill.com
    */
-  constructor(url) {
-    this.base = url || "http://animepill.com";
+  constructor(prefix = '', url) {
+    this.base = 'http://animepill.com' || url
+    this.prefix = prefix
   }
   // options can look like this
   // {limit: 50}
   _genOptions(options) {
-    let i = 0;
-    let res = "";
+    let i = 0
+    let res = ''
     for (let opt in options) {
       if (i === 0) {
-        res += `?${opt}=${options[opt]}`;
+        res += `?${opt}=${options[opt]}`
       } else {
-        res += `&${opt}=${options[opt]}`;
+        res += `&${opt}=${options[opt]}`
       }
-      i++;
+      i++
     }
-    return res;
+    return res
   }
 
   _parsePathParam(path, param) {
-    const reg = /{(.*?)}/g; // get the stuff between the {}
-    const arr = path.match(reg) || []; // array with the results of the regexp
+    const reg = /{(.*?)}/g // get the stuff between the {}
+    const arr = path.match(reg) || [] // array with the results of the regexp
     for (var i = 0; i < arr.length; i++) {
-      let par = param[arr[i].substr(1, arr[i].length-2)];
+      let par = param[arr[i].substr(1, arr[i].length - 2)]
       if (par) {
-        path = path.replace(arr[i], par);
+        path = path.replace(arr[i], par)
       } else {
-        path = path.replace(arr[i], "");
+        path = path.replace(arr[i], '')
       }
     }
 
-    path = path.replace(/\/\/+/g, "/"); // remove double slashes
-    return path;
+    path = path.replace(/\/\/+/g, '/') // remove double slashes
+    return path
   }
 
   _get(path, options, param) {
-    if(path.search(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/) != -1) {
+    if (
+      path.search(
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/,
+      ) != -1
+    ) {
       // url is passed
-      return fetch(path)
-        .then(res => res.text())
+      return fetch(path).then(res => res.text())
     }
-    return fetch(this.base + this._parsePathParam(path, param) + this._genOptions(options))
-      .then(res => res.text())
+    return fetch(
+      this.prefix +
+        this.base +
+        this._parsePathParam(path, param) +
+        this._genOptions(options),
+    ).then(res => res.text())
   }
 
   _getJson(path, options, param) {
-    return fetch(this.base + this._parsePathParam(path, param) + this._genOptions(options))
-      .then(res => res.json())
+    return fetch(
+      this.prefix +
+        this.base +
+        this._parsePathParam(path, param) +
+        this._genOptions(options),
+    ).then(res => res.json())
   }
 
   /**
@@ -63,16 +76,19 @@ class Anime {
    * @returns {Promise<Object[]>} an array with results
    */
   search(query) {
-    return this._getJson(endpoints.search, {q: query})
-      .then(res => {
-        return new Promise((resolve, reject) => {
-          const output = [];
-          for (let item of res.data) {
-            output.push(Object.assign({}, item, {getEpisodes: () => this.getEpisodes(item.slug)}));
-          }
-          resolve(output);
-        });
-      });
+    return this._getJson(endpoints.search, { q: query }).then(res => {
+      return new Promise((resolve, reject) => {
+        const output = []
+        for (let item of res.data) {
+          output.push(
+            Object.assign({}, item, {
+              getEpisodes: () => this.getEpisodes(item.slug),
+            }),
+          )
+        }
+        resolve(output)
+      })
+    })
   }
 
   /**
@@ -81,35 +97,42 @@ class Anime {
    * @returns {Promise<Object>} an object with all the episodes
    */
   getEpisodes(slug) {
-    return this._get(endpoints.eps, {}, {slug: slug})
-      .then(html => {
-        return new Promise((resolve, reject) => {
-          try {
-            const $ = cheerio.load(html);
-            const output = {};
-            $(".anime__episodes").children().each((i, elem) => {
-              elem = $(elem);
-              const titles = elem.text().trim().split("\"").map(x => x.trim()).filter(x => x != "");
-              const href = elem.find("a").attr("href");
-              let episode = titles[0].split(/([0-9]+)/)
-                .filter(x => x != "")
+    return this._get(endpoints.eps, {}, { slug: slug }).then(html => {
+      return new Promise((resolve, reject) => {
+        try {
+          const $ = cheerio.load(html)
+          const output = {}
+          $('.anime__episodes')
+            .children()
+            .each((i, elem) => {
+              elem = $(elem)
+              const titles = elem
+                .text()
+                .trim()
+                .split('"')
+                .map(x => x.trim())
+                .filter(x => x != '')
+              const href = elem.find('a').attr('href')
+              let episode = titles[0]
+                .split(/([0-9]+)/)
+                .filter(x => x != '')
                 .filter(x => !isNaN(x))
-                .map(x => parseInt(x))             
-              episode = episode[episode.length-1]
+                .map(x => parseInt(x))
+              episode = episode[episode.length - 1]
               output[episode] = {
                 titles: titles,
                 href: href,
                 episode: episode,
                 slug: slug,
-                getEpisode: () => this.getEpisode(href)
-              };
-            });
-            resolve(output);
-          } catch (e) {
-            reject(e);
-          }
-        });
-      });
+                getEpisode: () => this.getEpisode(href),
+              }
+            })
+          resolve(output)
+        } catch (e) {
+          reject(e)
+        }
+      })
+    })
   }
 
   /**
@@ -118,34 +141,42 @@ class Anime {
    * @returns {Promise<Object[]>} an array with the episode mp4 uri
    */
   getEpisode(slug, ep) {
-    let endpoint = endpoints.ep;
-    if(slug.search(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/) != -1) {
+    let endpoint = endpoints.ep
+    if (
+      slug.search(
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/,
+      ) != -1
+    ) {
       // url is passed
-      endpoint = slug;
+      endpoint = slug
     }
-    
-    return this._get(endpoint, {}, {slug: slug, episode: ep})
-      .then(html => {
-        const $ = cheerio.load(html);
-        return fetch($("#video-iframe").attr("src"))
-          .then(data => data.text())
-          .then(html => {
-            return new Promise((resolve, reject) => {
-              try {
-                const searchPart = "<script type='text/javascript'>eval(";
-                html = html.substring(html.indexOf(searchPart)+searchPart.length-1, html.length);
-                html = html.substring(0, html.indexOf("</script>"));
-                html = eval(html);
-                html = html.substring(html.indexOf("myVideo.src(")+"myVideo.src(".length, html.length-2);
-                resolve(eval(html));
-              } catch (e) {
-                reject(e);
-              }
-            });
-          })
-      });
-  }
 
+    return this._get(endpoint, {}, { slug: slug, episode: ep }).then(html => {
+      const $ = cheerio.load(html)
+      return fetch($('#video-iframe').attr('src'))
+        .then(data => data.text())
+        .then(html => {
+          return new Promise((resolve, reject) => {
+            try {
+              const searchPart = "<script type='text/javascript'>eval("
+              html = html.substring(
+                html.indexOf(searchPart) + searchPart.length - 1,
+                html.length,
+              )
+              html = html.substring(0, html.indexOf('</script>'))
+              html = eval(html)
+              html = html.substring(
+                html.indexOf('myVideo.src(') + 'myVideo.src('.length,
+                html.length - 2,
+              )
+              resolve(eval(html))
+            } catch (e) {
+              reject(e)
+            }
+          })
+        })
+    })
+  }
 }
 
-module.exports = Anime;
+module.exports = Anime
